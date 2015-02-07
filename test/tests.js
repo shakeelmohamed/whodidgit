edac047d76de442ba4087ac74929516da67b79be
Shakeel Mohamed <Contact@shakeelmohamed.com>
2015-02-07 13:51:33 -0800

    tests added

--- test/tests.js
+++ test/tests.js
@@ -12,7 +12,7 @@ var url = scheme + "://" + domain + ":" + port;
 
 var testUser = "shakeelmohamed";
 var testRepo = "whodidgit";
-var testCommit = ""; // FIXME: fill me in
+var testCommit = "c977b473efb8d1fa3bede0171b005bb4c00c1561"; // FIXME: update with a new commit
 
 describe("Home page", function() {
     describe("HTTP GET - no args", function() {
@@ -49,13 +49,98 @@ describe("Home page", function() {
     });
 });
 
-// All args as get/post
+describe("JSON response - example commit", function() {
+    var urlAllParms = url + "?owner=" + testUser + "&repo=" + testRepo + "&sha=" + testCommit;
+    var responseHeading = "GitHub gave us the following information about the author of that commit:";
+    var pattern = /\d+\sof\s\d+\srequests\sremaining\sthis\shour\./;
 
-// 1 of each arg as get/post
+    describe("HTTP GET - all args", function() {
+        var responseString = "";
+        var offset = responseHeading.length;
+        var apiStatus = "";
+        before(function(done) {
+            http.get(urlAllParms, function (res) {
+                assert.equal(200, res.statusCode);
+                res.on("data", function(chunk) {
+                    responseString = chunk.toString("utf8");
+                    done();
+                });
+            });
+        });
+        it("Should display a properly formatted heading", function() {
+            assert.ok(responseString);
+            assert.ok(responseString.length > 0);
+            assert.equal(0, responseString.indexOf(responseHeading));
+            assert.equal(" ", responseString.substring(offset, offset += 1));
+            assert.equal("\n", responseString.substring(offset, ++offset));
+        });
+        it("Should have a valid API status", function() {
+            assert.equal("\n", responseString.substring(responseString.length - 1, responseString.length));
+            
+            apiStatus = responseString.substring(responseString.substring(0, responseString.length-1).lastIndexOf("\n"), responseString.length-1);
+            assert.equal(1, apiStatus.match(pattern).length);
+        });
+        it("Should display properly formatted JSON", function() {
+            var theJSON = JSON.parse(responseString.substring(offset, responseString.lastIndexOf(apiStatus) - 1));
+            assert.ok(theJSON);
 
-// Form submission
+            var keys = ["name", "email", "date"];
+            assert.equal(3, keys.length);
 
-// Hardcoded commit get/post
+            for(var k in keys) {
+                assert.ok(theJSON.hasOwnProperty(keys[k]));
+                assert.ok(theJSON[keys[k]].length > 0);
+            }
+        });
+    });
+    describe("HTTP POST - all args", function() {
+        var responseString = "";
+        var offset = responseHeading.length;
+        var apiStatus = "";
+        var options = {
+            url: url,
+            method: "POST",
+            form: {
+                "owner": testUser,
+                "repo": testRepo,
+                "sha": testCommit
+            }
+        };
+        before(function(done) {
+            request(options, function (error, res, body) {
+                assert.ok(!error);
+                assert.equal(200, res.statusCode);
+                responseString = body;
+                done();
+            });
+        });
+        it("Should display a properly formatted heading", function() {
+            assert.ok(responseString);
+            assert.ok(responseString.length > 0);
+            assert.equal(0, responseString.indexOf(responseHeading));
+            assert.equal(" ", responseString.substring(offset, offset += 1));
+            assert.equal("\n", responseString.substring(offset, ++offset));
+        });
+        it("Should have a valid API status", function() {
+            assert.equal("\n", responseString.substring(responseString.length - 1, responseString.length));
+            
+            apiStatus = responseString.substring(responseString.substring(0, responseString.length-1).lastIndexOf("\n"), responseString.length-1);
+            assert.equal(1, apiStatus.match(pattern).length);
+        });
+        it("Should display properly formatted JSON", function() {
+            var theJSON = JSON.parse(responseString.substring(offset, responseString.lastIndexOf(apiStatus) - 1));
+            assert.ok(theJSON);
+
+            var keys = ["name", "email", "date"];
+            assert.equal(3, keys.length);
+
+            for(var k in keys) {
+                assert.ok(theJSON.hasOwnProperty(keys[k]));
+                assert.ok(theJSON[keys[k]].length > 0);
+            }
+        });
+    });
+});
 
 // Utils tests
 describe("Utils", function() {
